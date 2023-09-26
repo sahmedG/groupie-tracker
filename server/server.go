12871540561e17d437ff25df4ca0feb753047d74
= <=>
2023-09-26 17:53:44 +0300

    modded bands css

--- server/server.go
+++ server/server.go
@@ -3,10 +3,16 @@ package main
 import (
 	ConcertAPI "ConcertAPI/handlers"
 	"net/http"
+	"fmt"
+	"log"
 )
 
 func main() {
 	ConcertAPI.Parse()
 	var r ConcertAPI.Router
-	http.ListenAndServe(":8080", &r)
+	fmt.Println("Server runing on port 4200, goto localhost:4200")
+	err := http.ListenAndServe(":4200", &r)
+	if err != nil {
+		log.Fatal(err) //* logging any errors in the terminal
+	}
 }
